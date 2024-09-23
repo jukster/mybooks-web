f6dac2417751cd77380027a8e2fd94165d2dc372
Marko Jukić <marko.jukic@gmail.com>
2024-09-23 16:09:59 +0200

    Added welcome message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,18 @@
 // src/pages/index.js
 import BookList from '../components/BookList';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
-  return <BookList />;
+  const { data: session } = useSession();
+
+  return (
+    <div>
+      {session ? (
+        <h1>Welcome, {session.user.name}!</h1>
+      ) : (
+        <h1>Welcome, Guest!</h1>
+      )}
+      <BookList />
+    </div>
+  );
 }
