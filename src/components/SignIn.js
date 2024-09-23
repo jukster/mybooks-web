d6e9b4e43684688fcdcf46de9717bd2c9804ec48
Marko Jukić <marko.jukic@gmail.com>
2024-09-23 18:17:45 +0200

    Added Redirect options to route back correctly based on the environment

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,8 +4,15 @@ import { supabase } from '../supabaseClient';
 
 const SignIn = () => {
   const signInWithGoogle = async () => {
+    const redirectTo = process.env.NODE_ENV === 'production'
+    ? 'https://https://mybooks-web.vercel.app/'
+    : 'http://localhost:3000';
+
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+        redirectTo: redirectTo,
+      },
     });
     if (error) console.error('Error: ', error.message);
   };
